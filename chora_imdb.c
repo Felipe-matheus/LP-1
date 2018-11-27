b06577b23a16a4c7408c72c091a8abafae84f2d5
Felipe Matheus Costa <44986680+Felipe-matheus@users.noreply.github.com>
2018-11-27 11:07:04 -0200

    Update chora_imdb.c

--- chora_imdb.c
+++ chora_imdb.c
@@ -1,111 +1,107 @@
 #include<stdio.h>
+
 #include<string.h>
+
 #include<stdlib.h>
 
 typedef struct{
-		char nome[100];
-		int ano;
-		float nota;
-	}movie;
-	movie movies[100];
-	int n=0;
+	char nome[100];
+	int ano;
+	float nota;
+}movie;
+movie movies[100];
+int n;
+FILE *file;
 
-int load(char txt[50]){
-	FILE *archive;
-   	FILE *file;
-   char l[30];
-   int i, k = 0;
-   // Abrindo o arquivo.
-
-   file = fopen(txt,"r");
-
-
-   // Lendo os dados do arquivo e salvando no vetor.
-
-   while(!feof(file)){
-
-       fgets(movies[i].nome, 30, file);
-       fscanf(file,"%d",movies[i].ano, 30);
-       fscanf(file,"%f",movies[i].nota, 30);
-       fgets(l, 30, file);
-       fscanf(file, "%d", k);
-       i++;
-   }
-
-   fclose(file);
-
-   return 0;
+cadastrar(){
+	int i=0, op;
+	do{
+		fflush(stdin);
+		printf("\nInsira o nome do filme: ");
+		gets(movies[i].nome);
+		fflush(stdin);
+		printf("\nInsira o ano que o filme foi lancado: ");
+		scanf("%d", &movies[i].ano);
+		printf("\nInsira a nota do filme: ");
+		scanf("%f", &movies[i].nota);
+		i++;
+		system("cls");
+		fflush(stdin);
+		printf("\nDeseja inserir mais um filme: \n1-sim\t2-nao\n");
+		scanf("%d",&op);
+		system("cls");
+		
+		
+	}while(op==1);
+	n=i;
+}
+editar(){
+	int k,opc;
+	char nome[100];
+	do{
+		fflush(stdin);
+		printf("\nDeseja fazer alteracao em qual filme(insira o numero do indice dele na lista): ");
+		scanf("%d", &k);
+		fflush(stdin);
+		printf("\nQual o novo nome para o filme: ");
+		gets(nome);
+		strcpy(movies[k].nome,nome);
+		fflush(stdin);
+		printf("\nQual o ano desse novo filme: ");
+		scanf("%d", &movies[k].ano);
+		fflush(stdin);
+		printf("\nInsira a nova nota do filme: ");
+		scanf("%f", &movies[k].nota);
+		printf("\nDeseja editar mais um filme: \n1-sim\t2-nao\n");
+		scanf("%d",&opc);
+	}while(opc==1);
+	system("cls");
 
 }
-main(){
-	load("lista.txt");
-	FILE *archive;
-	int n;
-	archive = fopen("cont.txt", "r");
-	fscanf(archive, "%d", n, 1);
-	fclose(archive);
-	FILE *file;
-	int menu,i=0,op,opc,k;
-	char name[100];
-	do{
-		printf("\nInsira a opcao desejada: \n1-cadastrar\n2-editar\n3-listar\n4-deletar\n5-sair\n");
-		fflush(stdin);
-		scanf("%d", &menu);
-		switch(menu){
-			case 1:
-				do{
-					fflush(stdin);
-					printf("\nInsira o nome do filme: ");
-					gets(movies[n].nome);
-					fflush(stdin);
-					printf("\nInsira o ano em que o filme foi lancado: ");
-					scanf("%d", &movies[n].ano);
-					fflush(stdin);
-					printf("\nInsira a sua nota para o filme: ");
-					scanf("%f", &movies[n].nota);
-					fflush(stdin);
-					printf("\nDeseja adicionar mais um filme? \n1-sim\n2-nao\n");
-					scanf("%d", &op);
-					n++;
-					system("cls");
-				}while(op==1);
-				break;
-			case 2:
-				fflush(stdin);
-				printf("\nDeseja fazer alteracao em qual filme(insira o numero do indice dele na lista): ");
-				scanf("%d", &k);
-				fflush(stdin);
-				printf("\nQual o novo nome para o filme: ");
-				gets(name);
-				strcpy(movies[k].nome,name);
-				fflush(stdin);
-				printf("\nQual o ano desse novo filme: ");
-				scanf("%d", &movies[k].ano);
-				fflush(stdin);
-				printf("\nInsira a nova nota do filme: ");
-				scanf("%f", &movies[k].nota);
-				system("cls");
-				break;
-			case 3:
-				for(i=0;i<n;i++){
-					printf("\nnome: ");
-					puts(movies[i].nome);
-					printf("ano: %d", movies[i].ano);
-					printf("\nnota: %.2f", movies[i].nota);
-				}
-				break;
-				system("cls");
-		}
-	}while(menu!=5);
-	file = fopen("lista.txt", "w");
+
+listar(){
+	int i;
 	for(i=0;i<n;i++){
-		fprintf(file,"\n%s", movies[i].nome);
-		fprintf(file,"\n%d", movies[i].ano);
-		fprintf(file,"\n%.1f", movies[i].nota);
-	 }
+
+		printf("\nnome: ");
+
+		puts(movies[i].nome);
+
+		printf("ano: %d", movies[i].ano);
+
+		printf("\nnota: %.2f", movies[i].nota);
+		
+
+
+	}
+}
+
+
+
+main(){
+	int menu,j;
+	do{
+	printf("Insira a opção desejada: \n1-cadastrar\t2-editar\n3-listar\t4-sair\n");
+	scanf("%d", &menu);
+	system("cls");
+	switch(menu){
+		case 1:
+			cadastrar();
+			break;
+		case 2:
+			editar();
+			break;
+		case 3:
+			listar();
+			break;
+		
+	}
+	}while(menu<4 && menu>0);
+	file=fopen("filmes.txt","w");
+		for(j=0;j<n;j++){
+			fprintf(file,"%s\n",movies[j].nome);
+			fprintf(file,"%d\n", movies[j].ano);
+			fprintf(file, "%.1f\n", movies[j].nota);
+		}
 	fclose(file);
-	archive = fopen("cont.txt", "w");
-	fprintf(archive, "%d", n);
-	fclose(archive);
-	system("pause");
 }
